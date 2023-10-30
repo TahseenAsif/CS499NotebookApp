d6f5783dd23c3c3a3b450859d2e3cc953ee8a896
BrianKal <80415331+BrianKal@users.noreply.github.com>
2023-10-30 18:45:49 -0400

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -2,8 +2,9 @@ const { ipcRenderer } = require('electron');
         
 document.addEventListener('DOMContentLoaded', function () {
     const signInButton = document.getElementById('signInButton');
-    const signOutButton = document.getElementById('signOutButton');
+    // const signOutButton = document.getElementById('signOutButton');
     const signUpButton = document.getElementById('signUpButton');
+    const guestButton = document.getElementById('guest');
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
 
@@ -12,12 +13,17 @@ document.addEventListener('DOMContentLoaded', function () {
         const password = passwordInput.value;
         ipcRenderer.send('sign-in', email, password);
     });
+    /*
     signOutButton.addEventListener('click', () => {
         ipcRenderer.send('sign-out');
     });
+    */
     signUpButton.addEventListener('click', () => {
         const email = emailInput.value;
         const password = passwordInput.value;
         ipcRenderer.send('sign-up', email, password);
+    });
+    guestButton.addEventListener('click', () => {
+        ipcRenderer.send('guest-mode');
     });
 });
